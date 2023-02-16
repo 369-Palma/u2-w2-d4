250c478677af0bde1dd8624a3a05028d3213d62d
Palma Iacobelli <118762347+369-Palma@users.noreply.github.com>
2023-02-16 14:57:37 +0100

    Update index.js

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -5,7 +5,16 @@ const startState = {
 };
 
 const myReducer = (state = startState, action) => {
-  return;
+  switch (action.type) {
+    /* case "SALVA_AZIENDA":
+         return (
+            ...state,
+            favJobs: {...state.favJobs,
+            content:[...state.favJobs.content, action.payload]}
+        ) */
+    default:
+      return state;
+  }
 };
 
 export default myReducer;
